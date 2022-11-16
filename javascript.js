690f49e24355592514c4d78c2160ea586ada9016
Raiji-kun <huot.edouard@gmail.com>
2022-11-16 11:24:47 +0100

    html+alphabet

--- javascript.js
+++ javascript.js
@@ -11,3 +11,13 @@ const resultat = maj + underscore;
 const mot = document.getElementById('mot');
 mot.innerHTML += resultat; 
 
+const tabAlphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const block = document.getElementById('alphabet');
+
+
+tabAlphabet.forEach( x => {
+    var div = document.createElement('div');
+    div.setAttribute("id", "case");
+    div.textContent = x;
+    block.appendChild(div);
+});
